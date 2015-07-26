235827d8661dfed33a434ef50d8202f12848b325
Dusko Aleksic <dusko.sina.aleksic@gmail.com>
2015-07-26 18:53:11 +0200

    splash screen and sync data preference view

--- www/js/controllers/settings.js
+++ www/js/controllers/settings.js
@@ -2,21 +2,65 @@ angular.module('starter.settings', [])
 
 .controller('SettingsCtrl', function($scope, $ionicModal) {
 
+  $scope.splashScreenLength = 1;
+  $scope.syncDataTime = 15;
+
+  $scope.dragRangeSyncDataTime = function(value) {
+    $scope.syncDataTime= value;
+  };
+
+  $scope.dragRangeSplash = function(value) {
+    $scope.splashScreenLength = value;
+  };
+
   $ionicModal.fromTemplateUrl('../../templates/modal/theme.html', {
     scope: $scope,
     animation: 'slide-in-up'
   }).then(function(modal) {
-    $scope.modal = modal;
+    $scope.modalTheme = modal;
   });
-  $scope.openModal = function() {
-    $scope.modal.show();
+  $scope.openModalTheme = function() {
+    $scope.modalTheme.show();
   };
-  $scope.closeModal = function() {
-    $scope.modal.hide();
+  $scope.closeModalTheme = function() {
+    $scope.modalTheme.hide();
   };
+
+
+  $ionicModal.fromTemplateUrl('../../templates/modal/splashScreen.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modalSplash = modal;
+  });
+  $scope.openModalSplash = function() {
+    $scope.modalSplash.show();
+  };
+  $scope.closeModalSplash = function() {
+    $scope.modalSplash.hide();
+  };
+
+
+  $ionicModal.fromTemplateUrl('../../templates/modal/syncData.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modalSyncDataTime = modal;
+  });
+  $scope.openModalmodalSyncDataTime = function() {
+    $scope.modalSyncDataTime.show();
+  };
+  $scope.closeModalmodalSyncDataTime = function() {
+    $scope.modalSyncDataTime.hide();
+  };
+
+
+
   //Cleanup the modal when we're done with it!
   $scope.$on('$destroy', function() {
-    $scope.modal.remove();
+    $scope.modalTheme.remove();
+    $scope.modalSplash.remove();
+    $scope.modalSyncDataTime.remove()
   });
   // Execute action on hide modal
   $scope.$on('modal.hidden', function() {
@@ -27,7 +71,7 @@ angular.module('starter.settings', [])
     // Execute action
   });
 
-  
+
 
 
 });
